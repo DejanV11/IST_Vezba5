0e009de07aff476413dfb5992654748a497c29f9
unknown <dvasiljevic1998@gmail.com>
2020-04-12 16:53:32 +0200

    ListaKnjiga je dodat

---
+++ ListaKnjiga/knjige.js
@@ -0,0 +1,33 @@
+const knjige = [
+    {
+        id: 1,
+        naziv: "Vinetu",
+        autor: "Karl Maj"
+    },
+
+    {
+        id: 2,
+        naziv: "Vestac",
+        autor: "Andzej Sapkovski"
+    },
+
+    {
+        id: 3,
+        naziv: "Gospodar prstenova",
+        autor: "Dzon R. R. Tolkin"
+    },
+
+    {
+        id: 4,
+        naziv: "Besnilo",
+        autor: "Borislav Pekic"
+    },
+
+    {
+        id: 5,
+        naziv: "Ubistvo u Orijent ekspresu",
+        autor: "Agata Kristi"
+    }   
+];
+
+module.exports = knjige;
